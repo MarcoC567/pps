2c01ca9310e0193f98bad1b8f3c968142b9475b4
Cao Nguyen Duong Nguyen <caond.nguyen@gmail.com>
2025-04-12 12:55:30 +0200

    modified list to list dictionary

--- src/pages/purchase-disposition/const.tsx
+++ src/pages/purchase-disposition/const.tsx
@@ -5,43 +5,230 @@ export const items = [
   44, 45, 46, 47, 48, 52, 53, 57, 58, 59,
 ];
 
-export const usageRatioP1List = [
-  1, 0, 0, 7, 4, 2, 4, 3, 0, 0, 4, 1, 1, 1, 2, 1, 1, 2, 1, 3, 1, 1, 1, 2, 2, 72,
-  0, 0, 2,
-];
-
-export const usageRatioP2List = [
-  0, 1, 0, 7, 4, 2, 5, 3, 0, 0, 4, 1, 1, 1, 2, 1, 1, 2, 1, 3, 1, 1, 1, 2, 0, 0,
-  2, 72, 2,
-];
-
-export const usageRatioP3List = [
-  0, 0, 1, 7, 4, 2, 6, 3, 2, 72, 4, 1, 1, 1, 2, 1, 1, 2, 1, 3, 1, 1, 1, 2, 0, 0,
-  0, 0, 2,
-];
-
-export const discountQuantityValues = [
-  300, 300, 300, 6100, 3600, 1800, 4500, 2700, 900, 22000, 3600, 900, 900, 300,
-  1800, 900, 900, 1800, 2700, 900, 900, 900, 900, 1800, 600, 22000, 600, 22000,
-  1800,
+export const itemAdditionalData = [
+  {
+    itemNr: 21,
+    usageRatioP1: 1,
+    usageRatioP2: 0,
+    usageRatioP3: 0,
+    discountQuantity: 300,
+  },
+  {
+    itemNr: 22,
+    usageRatioP1: 0,
+    usageRatioP2: 1,
+    usageRatioP3: 0,
+    discountQuantity: 300,
+  },
+  {
+    itemNr: 23,
+    usageRatioP1: 0,
+    usageRatioP2: 0,
+    usageRatioP3: 1,
+    discountQuantity: 300,
+  },
+  {
+    itemNr: 24,
+    usageRatioP1: 7,
+    usageRatioP2: 7,
+    usageRatioP3: 7,
+    discountQuantity: 6100,
+  },
+  {
+    itemNr: 25,
+    usageRatioP1: 4,
+    usageRatioP2: 4,
+    usageRatioP3: 4,
+    discountQuantity: 3600,
+  },
+  {
+    itemNr: 27,
+    usageRatioP1: 2,
+    usageRatioP2: 2,
+    usageRatioP3: 2,
+    discountQuantity: 1800,
+  },
+  {
+    itemNr: 28,
+    usageRatioP1: 4,
+    usageRatioP2: 5,
+    usageRatioP3: 6,
+    discountQuantity: 4500,
+  },
+  {
+    itemNr: 32,
+    usageRatioP1: 3,
+    usageRatioP2: 3,
+    usageRatioP3: 3,
+    discountQuantity: 2700,
+  },
+  {
+    itemNr: 33,
+    usageRatioP1: 0,
+    usageRatioP2: 0,
+    usageRatioP3: 2,
+    discountQuantity: 900,
+  },
+  {
+    itemNr: 34,
+    usageRatioP1: 0,
+    usageRatioP2: 0,
+    usageRatioP3: 72,
+    discountQuantity: 22000,
+  },
+  {
+    itemNr: 35,
+    usageRatioP1: 4,
+    usageRatioP2: 4,
+    usageRatioP3: 4,
+    discountQuantity: 3600,
+  },
+  {
+    itemNr: 36,
+    usageRatioP1: 1,
+    usageRatioP2: 1,
+    usageRatioP3: 1,
+    discountQuantity: 900,
+  },
+  {
+    itemNr: 37,
+    usageRatioP1: 1,
+    usageRatioP2: 1,
+    usageRatioP3: 1,
+    discountQuantity: 900,
+  },
+  {
+    itemNr: 38,
+    usageRatioP1: 2,
+    usageRatioP2: 2,
+    usageRatioP3: 2,
+    discountQuantity: 1800,
+  },
+  {
+    itemNr: 39,
+    usageRatioP1: 1,
+    usageRatioP2: 1,
+    usageRatioP3: 1,
+    discountQuantity: 900,
+  },
+  {
+    itemNr: 40,
+    usageRatioP1: 1,
+    usageRatioP2: 1,
+    usageRatioP3: 1,
+    discountQuantity: 900,
+  },
+  {
+    itemNr: 41,
+    usageRatioP1: 1,
+    usageRatioP2: 1,
+    usageRatioP3: 1,
+    discountQuantity: 1800,
+  },
+  {
+    itemNr: 42,
+    usageRatioP1: 2,
+    usageRatioP2: 2,
+    usageRatioP3: 2,
+    discountQuantity: 2700,
+  },
+  {
+    itemNr: 43,
+    usageRatioP1: 1,
+    usageRatioP2: 1,
+    usageRatioP3: 1,
+    discountQuantity: 900,
+  },
+  {
+    itemNr: 44,
+    usageRatioP1: 1,
+    usageRatioP2: 1,
+    usageRatioP3: 1,
+    discountQuantity: 900,
+  },
+  {
+    itemNr: 45,
+    usageRatioP1: 1,
+    usageRatioP2: 1,
+    usageRatioP3: 1,
+    discountQuantity: 900,
+  },
+  {
+    itemNr: 46,
+    usageRatioP1: 1,
+    usageRatioP2: 1,
+    usageRatioP3: 1,
+    discountQuantity: 900,
+  },
+  {
+    itemNr: 47,
+    usageRatioP1: 1,
+    usageRatioP2: 1,
+    usageRatioP3: 1,
+    discountQuantity: 600,
+  },
+  {
+    itemNr: 48,
+    usageRatioP1: 1,
+    usageRatioP2: 1,
+    usageRatioP3: 1,
+    discountQuantity: 22000,
+  },
+  {
+    itemNr: 52,
+    usageRatioP1: 2,
+    usageRatioP2: 2,
+    usageRatioP3: 2,
+    discountQuantity: 600,
+  },
+  {
+    itemNr: 53,
+    usageRatioP1: 1,
+    usageRatioP2: 1,
+    usageRatioP3: 1,
+    discountQuantity: 22000,
+  },
+  {
+    itemNr: 57,
+    usageRatioP1: 1,
+    usageRatioP2: 1,
+    usageRatioP3: 1,
+    discountQuantity: 900,
+  },
+  {
+    itemNr: 58,
+    usageRatioP1: 1,
+    usageRatioP2: 1,
+    usageRatioP3: 1,
+    discountQuantity: 900,
+  },
+  {
+    itemNr: 59,
+    usageRatioP1: 1,
+    usageRatioP2: 1,
+    usageRatioP3: 1,
+    discountQuantity: 1800,
+  },
 ];
 
 const itemsBasicDataList = jsonData["artikel"];
 
-export const basicData = items.map((itemNr, index) => {
-  const itemBasicData = itemsBasicDataList.find((item) =>
-    item.artikelnummer.startsWith(itemNr.toString())
-  );
-  const deliveryTime = itemBasicData?.lieferzeit;
-  const deliveryTimeDeviation = itemBasicData?.lieferzeitabweichung;
+export const basicData = itemAdditionalData.map(
+  ({ itemNr, usageRatioP1, usageRatioP2, usageRatioP3, discountQuantity }) => {
+    const itemBasicData = itemsBasicDataList.find((item) =>
+      item.artikelnummer.startsWith(itemNr.toString())
+    );
+    const deliveryTime = itemBasicData?.lieferzeit;
+    const deliveryTimeDeviation = itemBasicData?.lieferzeitabweichung;
 
-  return {
-    itemNr: itemNr,
-    deliveryTime: deliveryTime,
-    deliveryTimeDeviation: deliveryTimeDeviation,
-    discountQuantity: discountQuantityValues[index],
-    usageRatioP1: usageRatioP1List[index],
-    usageRatioP2: usageRatioP2List[index],
-    usageRatioP3: usageRatioP3List[index],
-  };
-});
+    return {
+      itemNr,
+      deliveryTime,
+      deliveryTimeDeviation,
+      discountQuantity,
+      usageRatioP1,
+      usageRatioP2,
+      usageRatioP3,
+    };
+  }
+);
