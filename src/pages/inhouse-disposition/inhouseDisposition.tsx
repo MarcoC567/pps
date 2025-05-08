eb4707f9b74fd884bb499377801b60c655d098ea
Dennis Ginter <gide1011@h-ka.de>
2025-05-08 10:15:47 +0200

    fixed a few warnings

--- src/pages/inhouse-disposition/inhouseDisposition.tsx
+++ src/pages/inhouse-disposition/inhouseDisposition.tsx
@@ -1,12 +1,25 @@
-import { Box, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from "@mui/material"
+import {
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TableRow
+} from "@mui/material"
 import { DispositionValues, productBOMs } from "./util/bom.ts";
 import { PartId } from "./util/parts.type.ts";
 import { flattenTree } from "./util/helpers.ts";
+import { useNavigate } from "react-router-dom";
+import { useLanguage } from "../../context/LanguageContext.tsx";
 
-export const InhouseDisposition = ({ dpR, dpI }) => {
+export const InhouseDisposition = ({dpR, dpI}) => {
   const dpResult: Map<string, number> = dpR as Map<string, number>;
   const dpInput = dpI as Map<string, DispositionValues>;
   const flattenedBOMTree = flattenTree(productBOMs);
+  const {t} = useLanguage();
+  
   
   function getDispositionValues(partId: PartId): DispositionValues {
     return {
@@ -18,88 +31,104 @@ export const InhouseDisposition = ({ dpR, dpI }) => {
       productionOrder: dpResult.get(partId) ?? 0,
     };
   }
+  
   const fmt = (n: number) => Math.round(n).toLocaleString();
+  const navigate = useNavigate();
+  const handleNextClick = () => {
+    navigate("/production-order");
+  };
   //TODO: Translations
   
   return (
-    <TableContainer
-      sx={{
-        maxWidth: '100%',
-        minWidth: '80rem',
-        overflowX: 'auto',
-        boxShadow: 3,
-        borderRadius: 2,
-      }}
-    >
-      <Table size="small" stickyHeader>
-        <TableHead>
-          <TableRow
-            sx={{
-              backgroundColor: 'darkgoldenrod',
-              '& th': {
-                color: 'white',
-                fontWeight: 'bold',
-                fontSize: '1.0625rem',
-                position: 'sticky',
-                top: 0,
-                zIndex: 1,
+    <>
+      <TableContainer
+        sx={{
+          maxWidth: '100%',
+          minWidth: '80rem',
+          overflowX: 'auto',
+          boxShadow: 3,
+          borderRadius: 2,
+        }}
+      >
+        <Table size="small" stickyHeader>
+          <TableHead>
+            <TableRow
+              key="header"
+              sx={{
                 backgroundColor: 'darkgoldenrod',
-              },
-            }}
-          >
-            <TableCell>Part</TableCell>
-            <TableCell align="right">Demand</TableCell>
-            <TableCell align="right">Current Stock</TableCell>
-            <TableCell align="right">Planned Safety Stock</TableCell>
-            <TableCell align="right">Work in Progress</TableCell>
-            <TableCell align="right">Waiting Queue</TableCell>
-            <TableCell align="right">Production Order</TableCell>
-          </TableRow>
-        </TableHead>
-
-        
-        <TableBody>
-          {flattenedBOMTree.map((part, index) => {
-            const {
-              demand,
-              currentStock,
-              workInProgress,
-              waitingQueue,
-              productionOrder,
-              plannedSafetyStock,
-            } = getDispositionValues(part.partId);
-            
-            return (
-              <TableRow
-                key={part.id}
-                sx={{
-                  backgroundColor: index % 2 === 0 ? 'grey.50' : 'white',
-                  '&:last-child td, &:last-child th': { border: 0 },
-                  '&:hover': { backgroundColor: 'grey.100' },
-                }}
-              >
-                <TableCell>
-                  <Box pl={part.level * 2} fontWeight="medium">
-                    {part.partId + (["E16", "E17", "E26"].includes(part.partId) ? "*" : "") }
-                  </Box>
-                </TableCell>
-                <TableCell align="right">{fmt(demand)}</TableCell>
-                <TableCell align="right">{fmt(currentStock)}</TableCell>
-                <TableCell align="right">{fmt(plannedSafetyStock)}</TableCell>
-                <TableCell align="right">{fmt(workInProgress)}</TableCell>
-                <TableCell align="right">{fmt(waitingQueue)}</TableCell>
-                <TableCell align="right">{fmt(productionOrder??0)}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell>*Shared parts</TableCell>
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
-
+                '& th': {
+                  color: 'white',
+                  fontWeight: 'bold',
+                  fontSize: '1.0625rem',
+                  position: 'sticky',
+                  top: 0,
+                  zIndex: 1,
+                  backgroundColor: 'darkgoldenrod',
+                },
+              }}
+            >
+              <TableCell>Part</TableCell>
+              <TableCell align="right">Demand</TableCell>
+              <TableCell align="right">Current Stock</TableCell>
+              <TableCell align="right">Planned Safety Stock</TableCell>
+              <TableCell align="right">Work in Progress</TableCell>
+              <TableCell align="right">Waiting Queue</TableCell>
+              <TableCell align="right">Production Order</TableCell>
+            </TableRow>
+          </TableHead>
+          
+          
+          <TableBody>
+            {flattenedBOMTree.map((part, index) => {
+              const {
+                demand, currentStock, workInProgress, waitingQueue, productionOrder, plannedSafetyStock,
+              } = getDispositionValues(part.partId);
+              const rowKey = `${part.partId}-${index}`;
+              
+              return (
+                <TableRow
+                  key={rowKey}
+                  sx={{
+                    backgroundColor: index % 2 === 0 ? 'grey.50' : 'white',
+                    '&:last-child td, &:last-child th': {border: 0},
+                    '&:hover': {backgroundColor: 'grey.100'},
+                  }}
+                >
+                  <TableCell>
+                    <Box pl={part.level * 2} fontWeight="medium">
+                      {part.partId + (["E16", "E17", "E26"].includes(part.partId) ? "*" : "")}
+                    </Box>
+                  </TableCell>
+                  <TableCell align="right">{fmt(demand)}</TableCell>
+                  <TableCell align="right">{fmt(currentStock)}</TableCell>
+                  <TableCell align="right">{fmt(plannedSafetyStock)}</TableCell>
+                  <TableCell align="right">{fmt(workInProgress)}</TableCell>
+                  <TableCell align="right">{fmt(waitingQueue)}</TableCell>
+                  <TableCell align="right">{fmt(productionOrder ?? 0)}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell>*Shared parts</TableCell>
+              <TableCell/>
+              <TableCell/>
+              <TableCell align="right">
+                <button
+                  onClick={handleNextClick}
+                  className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-200 flex items-center gap-2 mx-auto "
+                >
+                  {t("next")}
+                </button>
+              </TableCell>
+              <TableCell/>
+              <TableCell/>
+              <TableCell/>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
